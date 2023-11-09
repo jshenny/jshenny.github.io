a540250399a306391eb720802d3c70dcd256a248
jshenny <jshen019@gmail.com>
2023-11-09 15:43:38 -0700

    added profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,19 @@
 import React from "react";
+import Me from "../assets/images/me.jpg"
 
 const About = () => {
     return (
-        <div className="body">
-            <img></img>
-            <p>hello! <br></br> <br></br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna erat, auctor vel ultrices ac,
-                facilisis ut sapien. Sed semper dui metus, eget scelerisque nulla cursus id. Proin sed eleifend ipsum.
-                Aenean auctor, eros vel dignissim posuere, dolor lorem congue mi, vitae convallis urna lorem ut arcu.
-                Sed nunc elit, commodo a nibh vulputate, convallis cursus felis. Orci varius natoque penatibus et magnis
-                dis parturient montes, nascetur ridiculus mus. Integer non odio viverra felis varius scelerisque.
-                Aliquam erat volutpat. Praesent risus ipsum, facilisis a ullamcorper non, volutpat eu lorem. Nam cursus
-                ligula nec nulla rutrum cursus. Suspendisse tellus ipsum, congue sed sem ac, pellentesque elementum nisl.
-                Nulla facilisi. Aliquam erat volutpat. Nulla pulvinar, tellus vel finibus laoreet, velit est rhoncus dolor,
-                sit amet convallis ligula leo ac augue. In hac habitasse platea dictumst. Aliquam ac est tellus.
-            </p>
+        <div className="flex flex-row gap-2 flex-wrap justify-between">
+            <div className="flex flex-col w-[300px] md:w-[40%] mb-6">            
+                <img src={Me} className="rounded"></img> 
+            </div>
+            <div className="flex flex-col w-[300px] md:w-[55%] mb-6">
+                <p> Hello! <br></br> <br></br>
+                    I'm a recent graduate from the University of Calgary studying computer science with a concentration in human-computer interactions. I love solving problems and 
+                    learning new things<br></br> <br></br>
+                    I like digital art, reading, playing video games and climbing in my free time.
+                </p>
+            </div>
         </div>
     )
 }
---
+++ src/components/Art.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Art  = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
